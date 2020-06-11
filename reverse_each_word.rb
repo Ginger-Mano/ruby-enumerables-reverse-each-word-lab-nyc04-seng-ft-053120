922404a1ec5ab5e5f7e352ba54715ba2ed2ed1a8
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-06-11 23:48:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,6 @@
 def reverse_each_word(sentence)
-<<<<<<< HEAD
   ary = sentence.split
   ary.collect do |words|
   words.reverse!
   end
   p ary.join(" ")
-=======
-  ary = []
-  ary << sentence
-  ary.each do |words|
-   words.reverse
-  end
->>>>>>> 9828e853760824b7d2acfe2864de821d26f1dcfc
-end
